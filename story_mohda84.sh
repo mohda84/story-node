cce5ba7217418f12f58b0a75e910c3f23695d2b4
mohda84 <139395938+mohda84@users.noreply.github.com>
2024-10-19 20:06:51 +0330

    Update story_mohda84.sh

--- story_mohda84.sh
+++ story_mohda84.sh
@@ -6,19 +6,19 @@ sudo apt install -y python3 python3-pip
 
 # Function to install Story automatically
 install_story() {
-    https://raw.githubusercontent.com/mohda84/story-node/407b75c2a3dbd27c89d23295bcea92477f7766e1/install
+  weget  https://raw.githubusercontent.com/mohda84/story-node/407b75c2a3dbd27c89d23295bcea92477f7766e1/install
     python3 story_installer.sh
 }
 
 # Function to check Story node synchronization status
 check_story_status() {
-    https://raw.githubusercontent.com/mohda84/story-node/588ad8b2c8379f553e11e656faed52463aabce72/status
+   weget https://raw.githubusercontent.com/mohda84/story-node/588ad8b2c8379f553e11e656faed52463aabce72/status
     python3 story_status
 }
 
 # Function to update Story to the latest version
 update_story() {
-    https://raw.githubusercontent.com/mohda84/story-node/1680cad701c6ef1b2987f22ede83a7d360756f61/update
+   weget https://raw.githubusercontent.com/mohda84/story-node/1680cad701c6ef1b2987f22ede83a7d360756f61/update
     python3 story_update
 }
 
